0c9a34228bb5af90684368af33085c46de4e0b50
cioan <cioan@pih.org>
2014-06-12 16:03:54 -0400

    use generic folders defined as variables

--- modules/pih_java/manifests/init.pp
+++ modules/pih_java/manifests/init.pp
@@ -1,20 +1,26 @@
 class pih_java {
     
-	$pih_java_home = 'c:\pih\java'
+	$pih_home = 'c:\pih'
+	$pih_java_home = "${pih_home}\\java"
+	$pih_java_zip = "${pih_home}\\jdk-6u45.zip"
 	
-	file { 'c:\pih\jdk-6u45.zip':
-		ensure  => file,
-		source	=> "puppet:///modules/pih_java/jdk-6u45.zip",				
+	file { $pih_home:
+		ensure  => directory,
 	}
-	
 	file { $pih_java_home:
 		ensure  => directory,
-		require => File['c:\pih\jdk-6u45.zip'],
+		require => File[$pih_home],
 	}
-
-	windows::unzip { 'c:\pih\jdk-6u45.zip':
+	
+	file { $pih_java_zip:
+		ensure  => file,
+		source	=> "puppet:///modules/pih_java/jdk-6u45.zip",		
+		require => File[$pih_java_home],
+	}
+	
+	windows::unzip { $pih_java_zip:
 		destination => $pih_java_home,
-		creates	=> 'c:\pih\java\bin',
+		creates	=> "${pih_java_home}\\bin",
 		require => File[$pih_java_home],
 	}
 }
